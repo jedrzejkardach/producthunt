ad11745d7e968d746693c560cfa8c81340e788da
heyoh5 heyoh5 <jedrzej.kardach@me.com>
2021-01-15 01:11:53 +0100

    add base.html and home view

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'products.apps.ProductsConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'producthunt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['producthunt/templates', 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
