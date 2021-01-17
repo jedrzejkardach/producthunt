efa9f8893113d8c7e54a3110c6fdcb174e256f91
heyoh5 heyoh5 <jedrzej.kardach@me.com>
2021-01-17 21:31:05 +0100

    add signup page (without logic)

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def signup(request):
+
+    return render(request=request, template_name='accounts/signup.html')
+
+
+def login(request):
+
+    return render(request=request, template_name='accounts/login.html')
+
+
+def logout(request):
+    # TODO Need to route to homepage
+    return render(request=request, template_name='accounts/signup.html')
