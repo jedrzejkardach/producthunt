7c743a4335a05560a567bfa7395d58d00d57ae8b
JJ <jedrzej@roomsage.com>
2021-01-27 01:24:34 +0100

    create product model and add ability to create products by user

--- products/models.py
+++ products/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+
+    title = models.CharField(max_length=100)
+    url = models.URLField(max_length=200)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    votes_total = models.IntegerField(default=1)
+    image = models.ImageField(upload_to="images/")
+    icon = models.ImageField(upload_to="images/")
+    body = models.TextField()
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+    # title
+    # url
+    # pub_date
+    # votes_total
+    # image
+    # icon
+    # body
+
+    def pub_date_pretty(self):
+        return self.pub_date
+
+    def summary(self):
+        str_body = str(self.body)
+        if len(str_body.split()) < 10:
+            return str_body
+        else:
+            return " ".join(str_body.split()[:10]) + "..."
+
+    def __str__(self):
+        return self.title
